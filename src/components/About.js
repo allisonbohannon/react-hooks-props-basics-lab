394eeda43917baa048f183ca8474884a61717d7b
Allison Bohannon <akboh.github@gmail.com>
2022-07-05 19:46:21 -0700

    commit tree bbb0bbbeb4f814c1313e0bad3c56d10070b34be7

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,16 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
+function About(props) {
+
+  const bioElement = <p>{props.bio}</p>
+
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {props.bio ? bioElement : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedIn={props.links.linkedin} />
     </div>
   );
 }
